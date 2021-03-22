5057ce003f2e51aa1567ec5173b421bbb70d8775
Gabriel Lima <gabriel.wprog@gmail.com>
2021-03-22 12:32:57 -0300

    Add next word fn

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,3 +1,4 @@
+use std::str::Utf8Error;
 use super::method::Method;
 use std::convert::TryFrom;
 use std::io::Read;
@@ -14,10 +15,30 @@ impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        match std::str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
-            Ok(request) => {},
-            Err(e) => return Err(e),
+        let request std::str::from_utf8(buf)?;
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
         }
+    }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    let mut iterator = request.chars();
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' {
+            return Some((&request[..i], &request[i+1..]))
+        }
+    }
+    None
+}
+
+impl From<Utf8Error> for ParseError {
+    from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 
